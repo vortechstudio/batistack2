c637e6ad52459c2e33bffb1dc885d62a5be337d1
maximemockelyn <maximemockelyn8@gmail.com>
2025-06-18 19:17:21 +0200

    refactor(layout): improve page styling and script loading
    - Add x-cloak style to prevent flash of unstyled content
    - Move app.js script loading to body for better performance
    - Add antialiased class for better text rendering

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -9,7 +9,12 @@
 
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+<style>
+    [x-cloak] {
+        display: none !important;
+    }
+</style>
 
 @filamentStyles
-@vite(['resources/css/app.css', 'resources/js/app.js'])
+@vite(['resources/css/app.css'])
 @fluxAppearance
