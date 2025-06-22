ff676f7f7e163d1a5121c12ac316f39aa6b0f29c
maximemockelyn <maximemockelyn8@gmail.com>
2025-06-22 20:39:12 +0200

    feat: add tauri desktop and mobile app setup with vite configs
    - Added Tauri configuration files and icons for desktop and mobile platforms
    - Created separate Vite configs for web, desktop, and mobile environments
    - Implemented Laravel commands to serve different environments (web, desktop, mobile)
    - Updated package.json with new scripts for Tauri and Vite environments
    - Added Tauri dependencies and build configuration

---
+++ app/Console/Commands/Server/ServeDesktopCommand.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Console\Commands\Server;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Process;
+use function Laravel\Prompts\intro;
+use function Laravel\Prompts\note;
+
+class ServeDesktopCommand extends Command
+{
+    protected $signature = 'serve:desktop';
+
+    protected $description = 'Command description';
+
+    public function handle()
+    {
+        intro( 'Running Desktop Environment' );
+
+        $this->initViteServer();
+        $this->initPHPServer();
+        $this->initTauriServer();
+    }
+
+    private function initTauriServer() : void
+    {
+        note( 'Starting Desktop App' );
+
+        if( ! File::exists( base_path( 'src-tauri/target' ) ) )
+        {
+            Process::path( 'src-tauri' )->forever()->run( "cargo build" );
+        }
+
+        Process::forever()->run( "npm run dev:tauri:desktop -- --port=50003" );
+    }
+
+    private function initViteServer() : void
+    {
+        note( "Starting Vite Development Server" );
+
+        Process::start( "npm run dev:vite:desktop" );
+    }
+
+    private function initPHPServer() : void
+    {
+        note( "Starting PHP Server" );
+
+        Process::start( "php artisan serve --port=50000" );
+    }
+}
---
+++ app/Console/Commands/Server/ServeMobileCommand.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Console\Commands\Server;
+
+use File;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Process;
+use Str;
+use function Laravel\Prompts\intro;
+use function Laravel\Prompts\note;
+use function Laravel\Prompts\warning;
+
+class ServeMobileCommand extends Command
+{
+    protected $signature = 'serve:mobile {--android} {--ios}';
+
+    protected $description = 'Command description';
+
+    public function handle()
+    {
+        if( ! $this->option( 'ios' ) && ! $this->option( 'android' ) )
+            warning( "A device option is needed : 'mobile:serve --android' or 'mobile:serve --ios'" );
+
+        intro( 'Running Mobile Environment' );
+
+        $this->initViteServer();
+        $this->initPHPServer();
+        $this->initTauriServer();
+    }
+
+    private function initTauriServer() : void
+    {
+        $device = $this->option( 'ios' ) ? 'ios' : 'android';
+
+        note( Str::headline( "Starting Mobile {$device} App" ) );
+
+        if( ! File::exists( base_path( 'src-tauri/target' ) ) )
+        {
+            Process::path( 'src-tauri' )->forever()->run( "cargo build" );
+        }
+
+        if( ! File::exists( base_path( "src-tauri/gen/{$device}" ) ) )
+        {
+            Process::forever()->run( "npm run tauri {$device} init" );
+        }
+
+        Process::forever()->run( "npm run dev:tauri:mobile:{$device} -- --port=50005" );
+    }
+
+    private function initViteServer() : void
+    {
+        note( "Starting Vite Development Server" );
+
+        Process::start( "npm run dev:vite:mobile" );
+    }
+
+    private function initPHPServer() : void
+    {
+        note( "Starting PHP Server" );
+
+        Process::start( "php artisan serve --port=50000" );
+    }
+}
