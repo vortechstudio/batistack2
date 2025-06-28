c245b5918378d8dc8c1348dc6555815c86767a4c
maximemockelyn <maximemockelyn8@gmail.com>
2025-06-28 15:24:34 +0200

    feat(table-bank): add CRUD actions for bank accounts
    - Implement create, edit and delete actions for bank accounts
    - Add validation for IBAN and BIC/SWIFT fields
    - Include bank selection dropdown with search functionality

--- app/Livewire/Tiers/Supply/Component/TableBank.php
+++ app/Livewire/Tiers/Supply/Component/TableBank.php
@@ -2,16 +2,27 @@
 
 namespace App\Livewire\Tiers\Supply\Component;
 
+use App\Models\Core\Bank;
 use App\Models\Tiers\Tiers;
+use App\Models\Tiers\TiersBank;
 use Filament\Actions\Concerns\InteractsWithActions;
 use Filament\Actions\Contracts\HasActions;
+use Filament\Actions\CreateAction;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\EditAction;
+use Filament\Forms\Components\Hidden;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Concerns\InteractsWithForms;
 use Filament\Forms\Contracts\HasForms;
+use Filament\Schemas\Components\Grid;
 use Filament\Tables\Columns\CheckboxColumn;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Concerns\InteractsWithTable;
 use Filament\Tables\Contracts\HasTable;
 use Filament\Tables\Table;
+use Intervention\Validation\Rules\Bic;
+use Intervention\Validation\Rules\Iban;
 use Livewire\Component;
 
 class TableBank extends Component implements HasTable, HasForms, HasActions
@@ -31,6 +42,66 @@ class TableBank extends Component implements HasTable, HasForms, HasActions
         return $table
             ->heading('Comptes Bancaires')
             ->query($this->tiers->banks()->with('bank')->getQuery())
+            ->headerActions([
+                CreateAction::make()
+                    ->schema([
+                        Select::make('bank_id')
+                            ->label('Banque')
+                            ->options(fn() => Bank::all()->pluck('name', 'id'))
+                            ->searchable(),
+
+                        Grid::make()
+                            ->schema([
+                                TextInput::make('iban')
+                                    ->label('IBAN')
+                                    ->rules([new Iban()])
+                                    ->required(),
+
+                                TextInput::make('bic')
+                                    ->label('BIC/SWIFT')
+                                    ->rules([new Bic()])
+                                    ->required(),
+                            ]),
+
+                        Hidden::make('tiers_id')
+                            ->default($this->tiers->id),
+                    ]),
+            ])
+            ->recordActions([
+                EditAction::make('edit')
+                    ->iconButton()
+                    ->icon('heroicon-o-pencil')
+                    ->schema([
+                        Select::make('bank_id')
+                            ->label('Banque')
+                            ->options(fn() => Bank::all()->pluck('name', 'id'))
+                            ->searchable(),
+
+                        Grid::make()
+                            ->schema([
+                                TextInput::make('iban')
+                                    ->label('IBAN')
+                                    ->rules([new Iban()])
+                                    ->required(),
+
+                                TextInput::make('bic')
+                                    ->label('BIC/SWIFT')
+                                    ->rules([new Bic()])
+                                    ->required(),
+                            ]),
+
+                        Hidden::make('tiers_id')
+                            ->default($this->tiers->id),
+                    ]),
+
+                DeleteAction::make('delete')
+                    ->icon('heroicon-o-trash')
+                    ->iconButton()
+                    ->color('danger')
+                    ->requiresConfirmation()
+                    ->modalHeading(fn(TiersBank $record) => "Supprimer ".$record->bank->name)
+                    ->action(fn(TiersBank $record) => $record->delete()),
+            ])
             ->columns([
                 TextColumn::make('bank.name')
                     ->label('Banque'),
@@ -45,7 +116,7 @@ class TableBank extends Component implements HasTable, HasForms, HasActions
                     ->label('ID Externe'),
 
                 CheckboxColumn::make('default')
-                ->label('Default'),
+                    ->label('Default'),
             ]);
     }
 
