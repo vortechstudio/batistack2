72683f9f14adb772ad35eb6477d16644341361c1
maximemockelyn <maximemockelyn8@gmail.com>
2025-07-13 20:31:44 +0200

    refactor(factories): reorganize employee factories into RH namespace
    - Move employee-related factories to RH namespace for better organization
    - Update factory data with more realistic values and enums
    - Add user creation logic in DatabaseSeeder for employees
    - Change default timezone to Europe/Paris in app config

---
+++ database/factories/RH/EmployeInfoFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories\RH;
+
+use App\Models\RH\Employe;
+use App\Models\RH\EmployeInfo;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Carbon;
+
+class EmployeInfoFactory extends Factory
+{
+    protected $model = EmployeInfo::class;
+
+    public function definition(): array
+    {
+        return [
+            'nationnality' => $this->faker->country,
+            'num_cni' => $this->faker->randomNumber(8),
+            'num_secu' => $this->faker->nir(),
+            'num_passport' => $this->faker->boolean ? $this->faker->randomNumber(8) : null,
+            'date_naissance' => Carbon::now()->subYears(rand(18, 60)),
+            'lieu_naissance' => $this->faker->city,
+            'pays_naissance' => $this->faker->country(),
+            'num_permis_btp' => $this->faker->boolean ? $this->faker->randomNumber(8) : null,
+            'exp_permis_btp' => $this->faker->boolean ? Carbon::now()->addYears(rand(1,5)) : null,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+
+            'employe_id' => Employe::factory(),
+        ];
+    }
+}
