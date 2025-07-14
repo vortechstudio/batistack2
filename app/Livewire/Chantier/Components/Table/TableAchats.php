0db3e357cf14869861755bb241af0a8507c4d237
maximemockelyn <maximemockelyn8@gmail.com>
2025-07-14 15:51:04 +0200

    fix(TableAchats): add create action for new expenses
    Implement a new create action in the TableAchats component allowing users to add new expenses with form fields for supplier, expense type, date, amount, description, invoice reference, and file uploads for supporting documents. Includes error handling and logging.

--- app/Livewire/Chantier/Components/Table/TableAchats.php
+++ app/Livewire/Chantier/Components/Table/TableAchats.php
@@ -2,20 +2,33 @@
 
 namespace App\Livewire\Chantier\Components\Table;
 
+use App\Enums\Chantiers\TypeDepenseChantier;
 use App\Models\Chantiers\ChantierDepense;
 use App\Models\Chantiers\Chantiers;
-
+use App\Models\Tiers\Tiers;
+use Carbon\Carbon;
+use Exception;
 use Filament\Actions\Concerns\InteractsWithActions;
 use Filament\Actions\Contracts\HasActions;
+use Filament\Actions\CreateAction;
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Concerns\InteractsWithForms;
 use Filament\Forms\Contracts\HasForms;
+use Filament\Schemas\Components\Grid;
+use Filament\Schemas\Components\Utilities\Get;
 use Filament\Tables\Columns\Summarizers\Sum;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Concerns\InteractsWithTable;
 use Filament\Tables\Contracts\HasTable;
 use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Table;
+use Illuminate\Support\Facades\Log;
 use Livewire\Component;
+use Livewire\Features\SupportFileUploads\TemporaryUploadedFile;
 
 class TableAchats extends Component implements HasTable, HasForms, HasActions
 {
@@ -27,6 +40,68 @@ class TableAchats extends Component implements HasTable, HasForms, HasActions
     {
         return $table
             ->query(ChantierDepense::query()->where('chantiers_id', $this->chantier->id))
+            ->headerActions([
+                CreateAction::make('new_achat')
+                    ->label('Nouvelle depense')
+                    ->modalHeading('Nouvelle depense')
+                    ->schema([
+                        Grid::make(4)
+                            ->schema([
+                                Select::make('tiers_id')
+                                    ->label('Fournisseur')
+                                    ->searchable()
+                                    ->options(Tiers::where('nature', 'fournisseur')->pluck('name', 'id')->toArray())
+                                    ->required(),
+
+                                Select::make('type_depense')
+                                    ->label('Type de depense')
+                                    ->options(TypeDepenseChantier::array())
+                                    ->required(),
+
+                                DatePicker::make('date_depense')
+                                    ->label('Date de depense')
+                                    ->required(),
+
+                                TextInput::make('montant')
+                                    ->label('Montant')
+                                    ->required(),
+                            ]),
+
+                            Textarea::make('description')
+                                ->required()
+                                ->label('Description'),
+
+                            TextInput::make('invoice_ref')
+                                ->label('Numero de facture'),
+
+                            FileUpload::make('justificatifs')
+                                ->label('Justificatifs')
+                                ->multiple()
+                                ->required()
+                                ->visibility('public')
+                                ->panelLayout('grid')
+                                ->directory('chantiers/'.$this->chantier->id.'/justificatifs')
+                                ->getUploadedFileNameForStorageUsing(function (TemporaryUploadedFile $file, Get $get) {
+                                    return "depense_".$get('tiers_id')."_".Carbon::createFromTimestamp(strtotime($get('date_depense')))->format('d_m_Y')."_".$get('type_depense').".".$file->getClientOriginalExtension();
+                                })
+                                ->disk('public'),
+                    ])
+                    ->using(function (array $data) {
+                        try {
+                            $this->chantier->depenses()->create([
+                                'description' => $data['description'],
+                                'montant' => $data['montant'],
+                                'date_depense' => $data['date_depense'],
+                                'type_depense' => $data['type_depense'],
+                                'invoice_ref' => $data['invoice_ref'],
+                                'tiers_id' => $data['tiers_id'],
+                                'chantiers_id' => $this->chantier->id,
+                            ]);
+                        } catch (Exception $ex) {
+                            Log::channel('github')->emergency($ex);
+                        }
+                    }),
+            ])
             ->columns([
                 TextColumn::make('description')
                     ->label('Description'),
