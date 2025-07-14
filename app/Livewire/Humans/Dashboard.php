ea3b78acb694c125f64b28b8cc8c9c72fad9b005
maximemockelyn <maximemockelyn8@gmail.com>
2025-07-14 17:45:41 +0200

    feat(humans/dashboard): add title and layout components to HR dashboard
    - Add #[Title] attribute to set page title
    - Implement grid layout with cards for leave balance and recent requests

--- app/Livewire/Humans/Dashboard.php
+++ app/Livewire/Humans/Dashboard.php
@@ -3,10 +3,12 @@
 namespace App\Livewire\Humans;
 
 use Livewire\Attributes\Layout;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
 class Dashboard extends Component
 {
+    #[Title('Espace RH')]
     #[Layout('components.layouts.humans')]
     public function render()
     {
--- resources/views/livewire/humans/dashboard.blade.php
+++ resources/views/livewire/humans/dashboard.blade.php
@@ -1,3 +1,15 @@
 <div>
-    {{-- Close your eyes. Count to one. That is how long forever feels. --}}
+    <div class="grid grid-cols-2 gap-5">
+        <x-mary-card class="bg-gray-100 mb-10" title="Congés" shadow separator>
+            Solde des congés en jours:
+        </x-mary-card>
+        <div>
+            <x-mary-card class="bg-gray-100 mb-10" title="Les 5 dernières demandes de congés" shadow separator>
+                Liste
+            </x-mary-card>
+            <x-mary-card class="bg-gray-100 mb-10" title="Les 5 dernières notes de frais modifiées" shadow separator>
+                Liste
+            </x-mary-card>
+        </div>
+    </div>
 </div>
