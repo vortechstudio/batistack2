afd79f2adc2b2e26c27ccf115f6f355affe7a7b5
maximemockelyn <maximemockelyn8@gmail.com>
2025-08-03 14:34:03 +0200

    fix(core): update notification styling and data fields
    Replace 'type' with 'status' for consistency and update styling classes. Use 'body' instead of 'description' field for notification content.

--- resources/views/livewire/core/block/notifications.blade.php
+++ resources/views/livewire/core/block/notifications.blade.php
@@ -8,14 +8,14 @@
     @if(count($notifications) > 0)
             <flux:menu class="w-1/4">
                 @foreach($notifications as $notification)
-                    <div class="flex justify-between items-center mx-2 my-4 p-2 bg-{{ $notification['data']['type'] }}-50 rounded">
+                    <div class="flex justify-between items-center mx-2 my-4 p-2 bg-{{ $notification['data']['status'] }} rounded">
                         <div class="flex items-center">
-                            <figure class="flex justify-center items-center border border-{{ $notification['data']['type'] }}-500 rounded-md w-12! h-12! bg-gray- me-3">
-                                @svg($this->getIconNotification($notification->id), 'text-'.$notification['data']['type'].'-500')
+                            <figure class="flex justify-center items-center border border-{{ $notification['data']['status'] }} rounded-md w-12! h-12! bg-gray-100 me-3">
+                                @svg($this->getIconNotification($notification->id), 'text-'.$notification['data']['status'])
                             </figure>
                             <div class="">
                                 <span class="font-bold text-lg">{{ $notification['data']['title'] }}</span><br>
-                                <p>{{ $notification['data']['description'] }}</p>
+                                <p>{!! $notification['data']['body'] !!}</p>
                             </div>
                         </div>
                         <flux:badge color="lime">{{ $notification->created_at->shortRelativeDiffForHumans() }}</flux:badge>
