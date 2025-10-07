7c56189b03aca6c2d4f9054abcaba708e4f5fa0a
Mockelyn Maxime <contact@vortechstudio.fr>
2025-10-07 14:47:45 +0200

    fix: handle exception when creating bridge user
    Add try-catch block to handle potential exceptions when creating a bridge user and updating the employee record. Sets bridge_user_id to null if the operation fails.

--- app/Helpers/RH/CreateEmploye.php
+++ app/Helpers/RH/CreateEmploye.php
@@ -78,13 +78,19 @@ final class CreateEmploye
             'status' => 'draft',
         ]);
 
-        $userBridge = $bridge->post('/aggregation/users', [
-            'external_user_id' => $salarie->matricule,
-        ]);
+        try {
+            $userBridge = $bridge->post('/aggregation/users', [
+                'external_user_id' => $salarie->matricule,
+            ]);
 
-        $salarie->update([
-            'bridge_user_id' => $userBridge['uuid'],
-        ]);
+            $salarie->update([
+                'bridge_user_id' => $userBridge['uuid'],
+            ]);
+        } catch (Exception $ex) {
+            $salarie->update([
+                'bridge_user_id' => null
+            ]);
+        }
 
         try {
             if (! Storage::disk('ged')->exists($salarie->matricule)) {
